46842810f601f8520b1a60cb0ef5c321fa36345a
Sophia Westwood <sophia@quip.com>
2016-04-15 18:27:11 -0700

    Add more Firebase demos, add nunjucks

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 /**
-	Run with npm start
-	
-	Whenever this file changes, you must stop and restart the server to pick up changes:
-		Type Cmd-C (or Ctrl-C) to stop, then type npm start to restart.
+    Run with npm start
+
+    Whenever this file changes, you must stop and restart the server to pick up changes:
+        Type Ctrl-C to stop, then type npm start to restart.
 */
+var nunjucks = require("nunjucks");
 var express = require("express");
 var app = express();
 
 app.set("port", (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));  // For our CSS, images, and Javascript
+
+// Use nunjucks (a templating language) to render views in case we ever want
+// something more fancy than plain HTML.
+// See http://stackoverflow.com/questions/19912389/how-to-use-html-in-express-framework-with-nunjucks-no-jade for examples
+nunjucks.configure("views", {
+  // Do some configuration
+  autoescape: true,
+  watch: true,
+  nocache: true,
+  express: app
+});
 
 app.get("/", function(request, response) {
-  response.render("pages/index.html");
+  response.render("index.html");
 });
 
 // We can name parameters by using a colon :
 // The parameter names get filled in on the request under request.params.
-app.get("/:filename", function(request, response) {
-  response.render("pages/" + request.params.filename + ".html");
+app.get("/:filename.html", function(request, response) {
+  response.render(request.params.filename + ".html");
 });
 
 app.listen(app.get("port"), function() {
   console.log("We're up and running! Open localhost:" + app.get("port") + " in your browser." +
-  	"\nBe sure to stop (Cmd-C or Ctrl-C) and rerun this command whenever you add new files or change the index.js code.");
+    "\nBe sure to stop (Ctrl-C) and rerun this command whenever you add new files or change the index.js code.");
 });
--- public/js/firebase-demo.js
+++ public/js/firebase-demo.js
@@ -1,38 +1,39 @@
-// Modified from https://www.firebase.com/tutorial/#gettingstarted
 
 // Replace with your own from https://www.firebase.com/account/#/
 FIREBASE_APP = "shining-inferno-8384";
 
-// See https://www.firebase.com/docs/web/guide/retrieving-data.html
-// and https://www.firebase.com/docs/web/guide/saving-data.html
-// for more!
-var firebase = new Firebase("https://" + FIREBASE_APP + ".firebaseio.com/");
+// See https://www.firebase.com/docs/web/guide/retrieving-data.html and
+// https://www.firebase.com/docs/web/guide/saving-data.html for more!
 
-ENTER_KEYCODE = 13;
+/* Chat room code */
+// Modified from https://www.firebase.com/tutorial/#gettingstarted
 
-// Send a message!
+var messagesRef = new Firebase("https://" + FIREBASE_APP + ".firebaseio.com/chatroom/messages");
+ENTER_KEYCODE = 13;  // Defined by the Powers That Be for the entire internet
+
 $(".message-input").keypress(function (e) {
+  // Send a message!
   if (e.keyCode == ENTER_KEYCODE) {
     var username = $(".name-input").val();
     var message = $(".message-input").val();
 
     // Save data to Firebase
-    firebase.push({name: username, text: message});
+    messagesRef.push({name: username, text: message, color: "orange"});
     // Empty the input since it is now submitted
     $(".message-input").val("");
   }
 });
 
-// Add a callback that is triggered for each chat message.
-firebase.limitToLast(10).on('child_added', function (snapshot) {
+messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+  // Triggered for each incoming chat messages
   // Get data
   var data = snapshot.val();
   var username = data.name || "anonymous";
   var message = data.text;
-
+  var color = data.color || "green";
   // Render the messages
   var messageElem = $("<li class='chat-message'>");
-  var nameElem = $("<span class='chat-username'></span> ")
+  var nameElem = $("<span class='chat-username' style='color: " + color + "'></span> ");
   nameElem.text(username + ":");
   messageElem.text(message).prepend(nameElem);
 
@@ -42,6 +43,39 @@ firebase.limitToLast(10).on('child_added', function (snapshot) {
   // Scroll to bottom of the message list
   messageList[0].scrollTop = messageList[0].scrollHeight;
 });
+
+
+/* Hit counter code */
+
+var hitCounterRef = new Firebase("https://" + FIREBASE_APP + ".firebaseio.com/hitCounter");
+
+$(".counter-button").click(function(e) {
+  // Update the counter
+  var currValue = parseInt($(".counter-hits").text());
+  hitCounterRef.set({"count": currValue += 1});
+});
+
+hitCounterRef.on("value", function(snapshot) {
+  // Receive a value from Firebase
+  $(".counter-hits").text(snapshot.val().count);
+});
+
+/* Moveable rectangle */
+
+var rectangleRef = new Firebase("https://" + FIREBASE_APP + ".firebaseio.com/rectangle");
+
+$(".rectangle-container").mousedown(function(e) {
+  // Update the counter
+  rectangleRef.set({"x": e.pageX, y: e.pageY});
+});
+
+rectangleRef.on("value", function(snapshot) {
+  // Receive a value from Firebase
+  var data = snapshot.val();
+  $(".rectangle").css("left", data.x);
+  $(".rectangle").css("top", data.y);
+});
+
 
 $(document).ready(function() {
 	console.log("Ready!");
